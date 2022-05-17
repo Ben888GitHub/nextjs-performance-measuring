a55f4e74cde19eed9cf5af892c027114db608ee6
Benedict <benedictryan80@gmail.com>
2022-05-17 18:42:18 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+export const reportWebVitals = (metric) => {
+	// console.log(metric);
+	metric.label === 'web-vital' && console.log(metric);
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
